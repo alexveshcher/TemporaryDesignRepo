916e224af34c7872ad3f61c70fc25b60abd1f45d
Oleksandr Veshcherovych <s.veshcherovych@gmail.com>
2017-05-03 23:51:17 +0300

    Sidebar hamburger & sublist extended

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,14 @@
-$("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("active");
-        $("#user-photo").toggleClass("hidden");
-        $("#show-info").toggleClass("hidden");
-        $(".collapsible-text").toggleClass("hidden")
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("active");
+    $("#user-photo").toggleClass("hidden");
+    $("#show-info").toggleClass("hidden");
+    $(".collapsible-text").toggleClass("hidden");
+    $("#control").toggleClass("glyphicon-menu-hamburger glyphicon-remove-circle");
+    $("#personal-options").toggleClass("hidden");
+});
+
+$("#personal").click(function (e) {
+    e.preventDefault();
+    $("#personal-options").toggleClass("sublist-hidden");
 });
